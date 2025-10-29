076154dcc2aa2cb139162ae90b12fdd05269ee7a
gemy3731 <mohamedgamalz3731@gmail.com>
2025-10-29 12:58:01 +0300

    init: init new project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/css/index.css";
+import { Dashboard, Login } from "./pages";
+
+function App() {
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Dashboard/>}/>
+    <Route path="/login" element={<Login/>}/>
+  </Routes>
+  </BrowserRouter>;
+}
+
+export default App;
